6d0f09d512a55079b67fab25968ac4097e041d9c
Eddie992 <ekataika2002@gmail.com>
2024-05-01 06:47:52 +0200

    initial commit

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# A puppet file that automates the creation of a custom header X-Served-By
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
